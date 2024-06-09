35608ee75fbd1ee1a440f804a8adbed192c10040
Crystal P <pittmanc82@gmail.com>
2024-06-08 21:48:31 -0700

    inventoriesReact node

---
+++ w1d1-homework/react-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Home from "./Home"
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import InventoryList from "./InventoryList"
+import InventoryEdit from "./InventoryEdit"
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+      
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/inventories' exact element={<InventoryList />} />
+          <Route path='/inventories/:id' element={<InventoryEdit />} />
+        </Routes>
+      
+      </BrowserRouter>
+    );
+  }
+}
+export default App;
